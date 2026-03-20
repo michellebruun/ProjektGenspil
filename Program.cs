8835555b966e081ab50561018995b930325cc889
Michelle <michellebruun97@gmail.com>
2026-03-20 10:17:34 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ProjektGenspil
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
